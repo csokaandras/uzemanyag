c2a05af03bc148865f13d3839079b702bb33f825
Csoki <80397154+csokaandras@users.noreply.github.com>
2024-04-23 13:43:25 +0200

    db

---
+++ uzemenyag_elszamolas.sql
@@ -0,0 +1,159 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2024. Ápr 23. 13:42
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `uzemenyag_elszamolas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `cars`
+--
+
+CREATE TABLE `cars` (
+  `ID` int(11) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `license` varchar(10) NOT NULL,
+  `consumption` int(11) NOT NULL,
+  `fuelID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `fuels`
+--
+
+CREATE TABLE `fuels` (
+  `ID` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `routes`
+--
+
+CREATE TABLE `routes` (
+  `ID` int(11) NOT NULL,
+  `start` varchar(50) NOT NULL,
+  `end` varchar(50) NOT NULL,
+  `km` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `userID` int(11) NOT NULL,
+  `carID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `pass` varchar(100) NOT NULL,
+  `perm` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `fuelID` (`fuelID`);
+
+--
+-- A tábla indexei `fuels`
+--
+ALTER TABLE `fuels`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- A tábla indexei `routes`
+--
+ALTER TABLE `routes`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `userID` (`userID`),
+  ADD KEY `carID` (`carID`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `perm` (`perm`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `fuels`
+--
+ALTER TABLE `fuels`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `routes`
+--
+ALTER TABLE `routes`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `cars`
+--
+ALTER TABLE `cars`
+  ADD CONSTRAINT `cars_ibfk_1` FOREIGN KEY (`fuelID`) REFERENCES `fuels` (`ID`);
+
+--
+-- Megkötések a táblához `routes`
+--
+ALTER TABLE `routes`
+  ADD CONSTRAINT `routes_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`),
+  ADD CONSTRAINT `routes_ibfk_3` FOREIGN KEY (`carID`) REFERENCES `cars` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
